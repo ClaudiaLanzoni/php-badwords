74360ac2915583b9e05e3bc6c48bf1a0dfbe5bba
ClaudiaLanzoni <claudia.lanzoni1991@gmail.com>
2021-10-12 18:42:56 +0200

    refactor - finalized exercise + str_word_count

--- index.php
+++ index.php
@@ -21,16 +21,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Php bad words</title>
 </head>
 <body>
+
     <h5>
-    <?php 
-        //echo $stringaDiprova . ' ' . 'Numero parole inserite: ' . str_word_count($stringaDiprova);
-       
-        echo $censura = str_replace($_GET["input"], $asterischiCensura, $stringaEsercizio);
-        
-    ?>
-    </h1>
+        <?php
+            echo $censura = str_replace($_GET["input"], $asterischiCensura, $stringaEsercizio);
+        ?>
+    </h5>
+
+    <h5>
+        <?php
+            echo 'Lunghezza testo inserito: ' . str_word_count($stringaEsercizio) . ' parole.'
+        ?>
+    </h5>
+
+    
 </body>
 </html>
